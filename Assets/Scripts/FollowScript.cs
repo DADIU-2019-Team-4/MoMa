922541a88be4fe8555b6e1743478a69eb6b68fba
Roxanne van Dam <roxanne.vandam@outlook.com>
2019-10-10 14:58:56 +0200

    tweaked some values

--- Assets/Scripts/FollowScript.cs
+++ Assets/Scripts/FollowScript.cs
@@ -11,7 +11,7 @@ public class FollowScript : MonoBehaviour
     private Vector3 velocity;
     public float dampTime = 0.3f;
     private Transform target;
-    static int numOfPastVelocities = 25;
+    static int numOfPastVelocities = 50;
     private Vector3[] pastVelocities = new Vector3[numOfPastVelocities];
     public float speed;
     static int numOfFutureFrames = 50;
@@ -20,6 +20,7 @@ public class FollowScript : MonoBehaviour
     private List<GameObject> futureDots = new List<GameObject>();
     private float timerValue = 0.2f;
     private float currentTimer;
+    private int framePerDot = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -63,11 +64,11 @@ public class FollowScript : MonoBehaviour
 
         pastDots.Clear();
 
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < numOfPastVelocities / framePerDot; i++)
         {
             GameObject dot = Instantiate(Dot);
             pastDots.Add(dot);
-            dot.transform.position = new Vector3(pastVelocities[5 * i].x, 0.1f, pastVelocities[5 * i].z);
+            dot.transform.position = new Vector3(pastVelocities[framePerDot * i].x, 0.1f, pastVelocities[framePerDot * i].z);
         }
     }
 
@@ -80,17 +81,20 @@ public class FollowScript : MonoBehaviour
 
         futureDots.Clear();
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < numOfFutureFrames / framePerDot; i++)
         {
             GameObject dot = Instantiate(Dot);
             futureDots.Add(dot);
-            dot.transform.position = new Vector3(futureVelocities[5 * i].x, 0.1f, futureVelocities[5 * i].z);
+            dot.transform.position = new Vector3(futureVelocities[framePerDot * i].x, 0.1f, futureVelocities[framePerDot * i].z);
         }
     }
 
     public void SavePastPositions(Vector3 inputVector)
     {
         Vector3.SmoothDamp(transform.position, (transform.position + inputVector), ref velocity, dampTime );
+        
+        if (inputVector == Vector3.zero)
+            velocity = Vector3.zero;
         
         transform.position += velocity * speed;
 

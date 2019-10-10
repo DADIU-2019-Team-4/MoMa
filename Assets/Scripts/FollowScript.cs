06636f1536deab558a2129dd3ca74eebd22a7c4f
Roxanne van Dam <roxanne.vandam@outlook.com>
2019-10-10 12:06:21 +0200

    made future dots working

--- Assets/Scripts/FollowScript.cs
+++ Assets/Scripts/FollowScript.cs
@@ -14,9 +14,10 @@ public class FollowScript : MonoBehaviour
     static int numOfPastVelocities = 5;
     private Vector3[] pastVelocities = new Vector3[numOfPastVelocities];
     public float speed;
-    static int numOfFutureFrames = 5;
+    static int numOfFutureFrames = 25;
     private Vector3[] futureVelocities = new Vector3[numOfFutureFrames];
-    private Queue<GameObject> dots = new Queue<GameObject>();
+    private Queue<GameObject> pastDots = new Queue<GameObject>();
+    private Queue<GameObject> futureDots = new Queue<GameObject>();
     private float timerValue = 0.2f;
     private float currentTimer;
 
@@ -35,40 +36,8 @@ public class FollowScript : MonoBehaviour
        // Vector3 inputVector = Vector3.Normalize(objectToFollow.transform.position - transform.position);
         Vector3 inputVector = GetComponent<MovementController>().GetInput();
         //velocity = inputVector * 8 * Time.deltaTime;
-        float newX = Mathf.SmoothDamp(transform.position.x, transform.position.x + inputVector.x, ref velocity.x, dampTime *Time.deltaTime);
-        float newY = Mathf.SmoothDamp(transform.position.y, transform.position.y + inputVector.y, ref velocity.y, dampTime * Time.deltaTime);
-        float newZ = Mathf.SmoothDamp(transform.position.z, transform.position.z + inputVector.z, ref velocity.z, dampTime * Time.deltaTime);
-        
-        //transform.position = new Vector3(newX, newY, newZ);
-        transform.position += velocity*speed;
-        //Debug.Log(velocity*speed);
 
-     
-        for (int i = numOfPastVelocities-1; i > 0; i--)
-        {
-            pastVelocities[i] = pastVelocities[i - 1];
-        }
-
-        if (currentTimer > timerValue)
-        {
-            GameObject dot = Instantiate(Dot);
-            dot.transform.position = new Vector3(pastVelocities[0].x, 0.1f, pastVelocities[0].z);
-            dots.Enqueue(dot);
-
-            if (dots.Count == numOfPastVelocities)
-            {
-                Destroy(dots.First());
-                dots.Dequeue();
-            }
-
-            currentTimer = 0;
-        }
-        else
-        {
-            currentTimer += Time.deltaTime;
-        }
-
-        pastVelocities[0] = (new Vector3(newX, newY, newZ));
+        SavePastPositions(inputVector);
         
         CalculateFuturePositions(inputVector);
     }
@@ -81,6 +50,69 @@ public class FollowScript : MonoBehaviour
             Gizmos.color = Color.red;
             Gizmos.DrawLine(transform.position, transform.position + new Vector3(50, 50, 50));
         }
+    }
+
+    public void DrawPastDots()
+    {
+        if (currentTimer > timerValue)
+        {
+            GameObject dot = Instantiate(Dot);
+            dot.transform.position = new Vector3(pastVelocities[0].x, 0.1f, pastVelocities[0].z);
+            pastDots.Enqueue(dot);
+
+            if (pastDots.Count == numOfPastVelocities)
+            {
+                Destroy(pastDots.First());
+                pastDots.Dequeue();
+            }
+
+            currentTimer = 0;
+        }
+        else
+        {
+            currentTimer += Time.deltaTime;
+        }
+    }
+
+    public void DrawFutureDots()
+    {
+        if (currentTimer > timerValue)
+        {
+            GameObject dot = Instantiate(Dot);
+            dot.transform.position = new Vector3(futureVelocities[24].x, 0.1f, futureVelocities[24].z);
+            futureDots.Enqueue(dot);
+
+            if (futureDots.Count == 5)
+            {
+                Destroy(futureDots.First());
+                futureDots.Dequeue();
+            }
+
+            currentTimer = 0;
+        }
+        else
+        {
+            currentTimer += Time.deltaTime;
+        }
+    }
+
+    public void SavePastPositions(Vector3 inputVector)
+    {
+        float newX = Mathf.SmoothDamp(transform.position.x, transform.position.x + inputVector.x, ref velocity.x, dampTime * Time.deltaTime);
+        float newY = Mathf.SmoothDamp(transform.position.y, transform.position.y + inputVector.y, ref velocity.y, dampTime * Time.deltaTime);
+        float newZ = Mathf.SmoothDamp(transform.position.z, transform.position.z + inputVector.z, ref velocity.z, dampTime * Time.deltaTime);
+
+        //transform.position = new Vector3(newX, newY, newZ);
+        transform.position += velocity * speed;
+
+        for (int i = numOfPastVelocities - 1; i > 0; i--)
+        {
+            pastVelocities[i] = pastVelocities[i - 1];
+        }
+
+        DrawPastDots();
+
+        pastVelocities[0] = (new Vector3(newX, newY, newZ));
     }
 
     public void CalculateFuturePositions(Vector3 inputVector)
@@ -98,6 +130,7 @@ public class FollowScript : MonoBehaviour
             futureVelocities[i] = futurePos = futurePos + futureVel * speed;
         }
 
+        DrawFutureDots();
         Debug.Log(futureVelocities[0].ToString());
     }
 
